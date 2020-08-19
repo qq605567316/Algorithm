cdd3255023e54d84be165c4ea31d41ebf1c45f78
tanjiang <tanjiang@meng-tu.com>
2020-08-19 23:46:15 +0800

    求第三大的数

--- src/main/java/org/example/ArrayAlgoManager.java
+++ src/main/java/org/example/ArrayAlgoManager.java
@@ -12,4 +12,39 @@ public class ArrayAlgoManager {
         return nums.length;
     }
 
+    //414. 第三大的数
+    public int thirdMax(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE, flag = 0;
+        boolean f = true;
+        for (Integer i : nums) {
+            if (i == Integer.MIN_VALUE && f) {
+                flag++;
+                f = false;
+            }
+            if (i > max1) {
+                flag++;
+                //原先第二大传递给第三大
+                max3 = max2;
+                //原先最大值传递给第二大
+                max2 = max1;
+                //更新最大值
+                max1 = i;
+            } else if (i > max2 && i < max1) {
+                flag++;
+                max3 = max2;
+                max2 = i;
+            } else if (nums[i] > max3 && i < max2) {
+                flag++;
+                max3 = i;
+            }
+        }
+        return flag >= 3 ? max3 : max1;
+    }
+
 }
--- src/test/java/org/example/ArrayAlgoManagerTest.java
+++ src/test/java/org/example/ArrayAlgoManagerTest.java
@@ -17,4 +17,10 @@ public class ArrayAlgoManagerTest {
         int[] data = {0,1,2,3,4,5,6,7,9};
         System.out.println(manager.missingNumber(data));
     }
+
+    @Test
+    public void thirdMax(){
+        int[] data = {2, 2, 3, 1};
+        System.out.println(manager.thirdMax(data));
+    }
 }
